e2ef081e72b72848d8a32dc66b3437f33bfc4b30
zaxwebs <zifidigital@gmail.com>
2023-04-24 00:53:41 +0530

    switched to imported prisma client

--- src/lib/server/lucia.js
+++ src/lib/server/lucia.js
@@ -1,11 +1,11 @@
 import lucia from "lucia-auth";
 import { sveltekit } from "lucia-auth/middleware";
 import prisma from "@lucia-auth/adapter-prisma";
-import { PrismaClient } from "@prisma/client";
+import PrismaClient from "$lib/server/prisma";
 import { dev } from "$app/environment";
 
 export const auth = lucia({
-	adapter: prisma(new PrismaClient()),
+	adapter: prisma(PrismaClient),
 	env: dev ? "DEV" : "PROD",
 	middleware: sveltekit()
 });
